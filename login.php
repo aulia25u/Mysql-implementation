e770916f52a1d703a1dcffaa0d015ec42c403b5b
Aulia Rahman <56797963+auliatenshi@users.noreply.github.com>
2020-12-21 22:52:47 +0700

    ADD url register

--- login.php
+++ login.php
@@ -100,7 +100,7 @@
 					</div>
 
 					<div class="text-center p-t-12">
-						<a class="txt2" href="#">
+						<a class="txt2" href="registration.php">
 							Create your Account
 							<i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
 						</a>
