436f45e3d71713db4a7c9b26f847f9994738fb2e
Aulia Rahman <56797963+auliatenshi@users.noreply.github.com>
2020-12-22 12:54:59 +0700

    Change Image to Nepi Phelia haha

--- recovery.php
+++ recovery.php
@@ -26,7 +26,7 @@
 		<div class="container-login100">
 			<div class="wrap-login100">
 				<div class="login100-pic js-tilt" data-tilt>
-					<img src="images/img-01.png" alt="IMG">
+					<img src="images/img-02.png" alt="IMG">
 				</div>
 
     <form class="login100-form validate-form" method="post" name="login">
--- registration.php
+++ registration.php
@@ -26,7 +26,7 @@
 		<div class="container-login100">
 			<div class="wrap-login100">
 				<div class="login100-pic js-tilt" data-tilt>
-					<img src="images/img-01.png" alt="IMG">
+					<img src="images/img-03.png" alt="IMG">
 				</div>
 </head>
 <body>
