b7663aa3c910e7d3083209ef80761fb4513b1059
Aulia Rahman <56797963+auliatenshi@users.noreply.github.com>
2020-12-22 22:43:46 +0700

    Testing fungsi reset password

--- recovery.php
+++ recovery.php
@@ -28,31 +28,99 @@
 				<div class="login100-pic js-tilt" data-tilt>
 					<img src="images/img-02.png" alt="IMG">
 				</div>
-
-    <form class="login100-form validate-form" method="post" name="login">
+				<?php
+include('db.php');
+if(isset($_POST["email"]) && (!empty($_POST["email"]))){
+$email = $_POST["email"];
+$email = filter_var($email, FILTER_SANITIZE_EMAIL);
+$email = filter_var($email, FILTER_VALIDATE_EMAIL);
+if (!$email) {
+   $error .="<p>Invalid email address please type a valid email address!</p>";
+   }else{
+   $sel_query = "SELECT * FROM `users` WHERE email='".$email."'";
+   $results = mysqli_query($con,$sel_query);
+   $row = mysqli_num_rows($results);
+   if ($row==""){
+   $error .= "<p>No user is registered with this email address!</p>";
+   }
+  }
+   if($error!=""){
+   echo "<div class='error'>".$error."</div>
+   <br /><a href='javascript:history.go(-1)'>Go Back</a>";
+   }else{
+   $expFormat = mktime(
+   date("H"), date("i"), date("s"), date("m") ,date("d")+1, date("Y")
+   );
+   $expDate = date("Y-m-d H:i:s",$expFormat);
+   $key = md5(2418*2+$email);
+   $addKey = substr(md5(uniqid(rand(),1)),3,10);
+   $key = $key . $addKey;
+// Insert Temp Table
+mysqli_query($con,
+"INSERT INTO `password_reset_temp` (`email`, `key`, `expDate`)
+VALUES ('".$email."', '".$key."', '".$expDate."');");
+ 
+$output='<p>Dear user,</p>';
+$output.='<p>Please click on the following link to reset your password.</p>';
+$output.='<p>-------------------------------------------------------------</p>';
+$output.='<p><a href="https://tubes.academia.fun/reset-password.php?
+key='.$key.'&email='.$email.'&action=reset" target="_blank">
+https://tubes.academia.fun/reset-password.php
+?key='.$key.'&email='.$email.'&action=reset</a></p>'; 
+$output.='<p>-------------------------------------------------------------</p>';
+$output.='<p>Please be sure to copy the entire link into your browser.
+The link will expire after 1 day for security reason.</p>';
+$output.='<p>If you did not request this forgotten password email, no action 
+is needed, your password will not be reset. However, you may want to log into 
+your account and change your security password as someone may have guessed it.</p>';   
+$output.='<p>Thanks,</p>';
+$output.='<p>Academia Team</p>';
+$body = $output; 
+$subject = "Password Recovery - tubes.academia.fun";
+ 
+$email_to = $email;
+$fromserver = "noreply@academia.fun"; 
+require("PHPMailer/PHPMailerAutoload.php");
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->Host = "mail.academia.fun"; // Enter your host here
+$mail->SMTPAuth = true;
+$mail->Username = "noreply@academia.fun"; // Enter your email here
+$mail->Password = "Mbl21sync!!@"; //Enter your password here
+$mail->Port = 587;
+$mail->IsHTML(true);
+$mail->From = "noreply@academia.fun";
+$mail->FromName = "tubes.academia.fun";
+$mail->Sender = $fromserver; // indicates ReturnPath header
+$mail->Subject = $subject;
+$mail->Body = $body;
+$mail->AddAddress($email_to);
+if(!$mail->Send()){
+echo "Mailer Error: " . $mail->ErrorInfo;
+}else{
+echo "<div class='error'>
+<p>An email has been sent to you with instructions on how to reset your password.</p>
+</div><br /><br /><br />";
+ }
+   }
+}else{
+?>
+    <form class="login100-form validate-form" method="post" name="reset">
 	<span class="login100-form-title">
 						Member Recovery
 					</span>
        
-	   <div class="wrap-input100 validate-input" data-validate = "Username Is Required">
-						<input class="input100" type="text" name="username" placeholder="Username">
-						<span class="focus-input100"></span>
-						<span class="symbol-input100">
-							<i class="fa fa-user" aria-hidden="true"></i>
-						</span>
-					</div>
-	   
       <div class="wrap-input100 validate-input" data-validate = "Email is required">
-						<input class="input100" type="text" name="email" placeholder="Email">
+						<input class="input100" type="email" name="email" placeholder="Email">
 						<span class="focus-input100"></span>
 						<span class="symbol-input100">
-							<i class="fa fa-lock" aria-hidden="true"></i>
+							<i class="fa fa-envelope" aria-hidden="true"></i>
 						</span>
 					</div>
 					
 					<div class="container-login100-form-btn">
-						<button class="login100-form-btn" type="submit" value="recovery" name="submit">
-							Login
+						<button class="login100-form-btn" type="submit" value="Reset Password" name="submit">
+						Recovery
 						</button>
 					</div>
 
@@ -64,13 +132,14 @@
 					</div>	
 					<br><br>
   </form>
-  
+
+  <p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<?php } ?>
 </div>
 		</div>
 	</div>
-	
-	
-
 	
 <!--===============================================================================================-->	
 	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
