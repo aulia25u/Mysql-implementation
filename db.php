3821686c23f77893b794bbd16a309277bfe3d575
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2020-12-25 22:47:12 +0700

    ga ubah apa2

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
-    // Enter your host name, database username, password, and database name.
-    // If you have not set database password on localhost then set empty.
-    $con = mysqli_connect("localhost","academia_tubes","Mbl21sync","academia_login");
-    // Check connection
-    if (mysqli_connect_errno()){
-        echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    }
-?>
+// Enter your host name, database username, password, and database name.
+// If you have not set database password on localhost then set empty.
+$con = mysqli_connect("localhost", "academia_tubes", "Mbl21sync", "academia_login");
+// $con = mysqli_connect("localhost", "root", "", "academia_tubes");
+// Check connection
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
